44811fce1d5990495b8a6a7132b206a7d4486f9b
Thejas <thejasbm@techis.io>
2022-04-08 11:33:36 +0530

    completed

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Routes , Route } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import Header from "./containers/Header";
+import "./App.css";
+import ProductDetail from "./containers/ProductDetail";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path = { "/" } element = { <ProductListing/> } />
+          <Route path = { "/product/:productId" } element = { <ProductDetail/> } />
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
